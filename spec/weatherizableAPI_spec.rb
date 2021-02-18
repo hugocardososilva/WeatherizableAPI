c7bc66cd0c0da0360ccfe9c73ac95e7c7599563d
Hugo Silva <hugocardososilva@gmail.com>
2021-02-18 01:22:48 -0300

    build(CI): first commit

---
+++ spec/weatherizableAPI_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe WeatherizableAPI do
+  it "has a version number" do
+    expect(WeatherizableAPI::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
